67a9b03940d12e7ec490b79c91aaf3aa3f771bbb
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-26 22:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let firstInput = prompt("Enter the first number");
+let first = firstInput*2;
+let secondInput = prompt("Enter the second number");
+
+let result = first+secondInput;
+
+return ("The final result is: "+result);
+
